403b7678862dee48b0a5764e048f3377b1b33643
Noel Burton-Krahn <noel@burton-krahn.com>
2016-12-06 15:25:49 -0800

    static files

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 #! /usr/bin/env python
+import datetime
 
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, send_from_directory
+
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return """
+    <h1>Hello OpenShift</h1>
+
+    <p>From noelbk at %(now)s.</p>
+
+    <p>Deployed on Openshift at <a href="%(url)s">%(url)s</a>.</p>
+    
+    <h2>Serving static Files</h2>
+    <ul>
+    <li><a href="/static/2014-dragons.html">Static Dragons</a> from <a href="http://js1k.com/">js1k</a>
+    </ul>
+    """ % dict(
+        now=datetime.datetime.now(),
+        url="http://cc4-project1-python2-cc4-project1.44fs.preview.openshiftapps.com",
+        )
+
+@app.route('/static/<path:path>')
+def static_files(path):
+    return send_from_directory('static', path)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
